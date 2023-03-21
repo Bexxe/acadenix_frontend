7e17a155d2ffffd4a3442554f0a473b7047fa973
Bexxe <116841733+Bexxe@users.noreply.github.com>
2023-03-21 04:27:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,13 @@
-const multipleItemCarousel = document.querySelector("#carouselExampleControls");
+var expanded = false;
 
-if (window.matchMedia("(min-width:576px)").matches) {
-  const carousel = new bootstrap.Carousel(multipleItemCarousel, {
-    interval: false
-  });
-
-  var carouselWidth = $(".carousel-inner")[0].scrollWidth;
-  var cardWidth = $(".carousel-item").width();
-
-  var scrollPosition = 0;
-
-  $(".carousel-control-next").on("click", function () {
-    if (scrollPosition < carouselWidth - cardWidth * 4) {
-      console.log("next");
-      scrollPosition = scrollPosition + cardWidth;
-      $(".carousel-inner").animate({ scrollLeft: scrollPosition }, 600);
-    }
-  });
-  $(".carousel-control-prev").on("click", function () {
-    if (scrollPosition > 0) {
-      console.log("prev");
-      scrollPosition = scrollPosition - cardWidth;
-      $(".carousel-inner").animate({ scrollLeft: scrollPosition }, 600);
-    }
-  });
-} else {
-  $(multipleItemCarousel).addClass("slide");
+function showCheckboxes() {
+  var checkboxes = document.getElementById("checkboxes");
+  if (!expanded) {
+    checkboxes.style.display = "block";
+    expanded = true;
+    checkboxes.style.maxHeight="160px";
+  } else {
+    checkboxes.style.display = "none";
+    expanded = false;
+  }
 }
