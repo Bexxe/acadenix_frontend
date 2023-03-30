dd1a1bcfd3989f4dec1c09948c612f37d7e56639
Bexxe <116841733+Bexxe@users.noreply.github.com>
2023-03-30 08:03:22 +0300

    Delete js directory

--- js/dene.js
+++
@@ -1,8 +0,0 @@
-function ileri(){
-    document.getElementById("cardlar2").style.display="inline";
-    document.getElementById("cardlar1").style.display="none";
-}
-function ger(){
-    document.getElementById("cardlar2").style.display="none";
-    document.getElementById("cardlar1").style.display="inline";
-}
